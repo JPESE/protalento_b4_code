7c71a32fc0ff0156bb0c3dd5b63f89b6017e545a
JOHANNA <pesemmarea@hotmail.com>
2023-01-18 22:55:50 -0500

    ejercicios clon y Js

---
+++ 2.2.2_condicionales/script.js
@@ -0,0 +1,53 @@
+/*a.Solicitar al usuario un número, y determinar si es divisible entre dos o no. Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x núm*/
+
+ //let numeroIngresado  = prompt( "ingresa un numero por favor" )
+
+  //if ( (numeroIngresado % 2) == 0 ) {
+  //alert(numeroIngresado + ' número es divisible entre 2')} 
+   //else {
+  //alert(numeroIngresado + ' número no es divisible entre 2')  }
+  
+/*b.Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en un Alert. */
+
+  //let numero = prompt("introduce un número entero"); 
+  //let resultado = parImpar (numero);
+  //alert ('El numero' +numero+ 'es' +resultado );
+  //console.log (numero);
+
+//function parImpar (numero) {
+  //if (numero % 2 == 0) 
+  //{ return ' par ';}
+  //else {return ' impar ';}
+//}
+//c.Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir "Ganaste un premio", en caso contrario mostrar el número y el mensaje “Lo sentimos, sigue participando”.
+//
+//let ValorCliente =prompt("Ingresa un numero de usuario");
+  //if((ValorCliente == 1000))
+    //{alert(ValorCliente + ' Ganaste un premio. ')}
+  //else{alert(ValorCliente + ' Lo sentimos, sigue participando. ')}
+//
+//
+//*d.Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es menor. No considerar el caso en que ambos números son iguales.
+//
+//    let a =prompt("Ingrese el primer numero");
+//    let b =prompt("Ingrese el segundo numero");
+          
+//            if(a>b)
+//           alert("El numero menor es : "+b);
+//            if(b>a)
+//             alert("El numero menor es : "+a);
+
+ //e.Solicitar al usuario que ingrese tres números y mostrar cuál de los tres es el número mayor. Considerar el caso en que 2 números sean iguales.
+ 
+ //     let a =prompt("ingrese el primer numero");
+ //   let b =prompt("ingrese el segundo numero");
+ //     let c =prompt("ingrese el tercer numero");
+ //     let d =prompt("numero mayor")
+ //       
+ //        if (a<=b && b<=c && c<=a)
+ //        alert("numero mayor: ");
+        
+
+
+ //Adicional: Requerir al usuario que ingrese un día de la semana e imprimir un mensaje si es lunes, otro mensaje diferente si es viernes, otro mensaje diferente si es sábado o domingo. Si el día ingresado no es ninguno de esos, imprimir otro mensaje.
+                
