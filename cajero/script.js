f532bb8fc237f4c4a77d402c8c086433d21552c7
JOHANNA <pesemmarea@hotmail.com>
2023-02-05 23:15:42 -0500

    estilos cajero

---
+++ cajero/script.js
@@ -0,0 +1,17 @@
+let usuario = [
+    {nombre: "MALI", saldo:200, password:1},{nombre:"GERA", saldo:290, password:2},
+    {nombre:"MAUI", saldo:67, password:3}
+];
+//variables globales
+let seleccionUsuario;
+let contraseña;
+let saldo;
+let ingresaMonto;
+let retiro;
+//funciones 
+    function mali () {
+        seleccionUsuario = usuario[0].nombre;
+        contraseña = usuario[0].saldo;
+        contraseña = usuario[0].password;
+}
+
