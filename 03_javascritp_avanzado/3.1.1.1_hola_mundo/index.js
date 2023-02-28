299557c9f032615540aea64eeafd3c2830c30093
JOHANNA <pesemmarea@hotmail.com>
2023-02-27 21:53:18 -0500

    terminados ejecicios en casa node.js

--- 03_javascritp_avanzado/3.1.1.1_hola_mundo/index.js
+++ 03_javascritp_avanzado/3.1.1.1_hola_mundo/index.js
@@ -1,2 +1,10 @@
-suma = 3 + 5;
-console.log("Hello word!!", suma);
+//suma = 3 + 5;
+//console.log("Hello word!!", suma);
+
+//EJERCICIO 1.
+//numero = 3 > 4;
+//console.log ("numero mayor", numero);
+
+//EJERCICIO 2.
+//numero = 1000 ;
+//console.log ("Ganaste", numero);
