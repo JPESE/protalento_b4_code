f73ab4fb5c130c672c07d245901fde5c6325587a
JOHANNA <pesemmarea@hotmail.com>
2023-03-07 22:56:52 -0500

    ejercicios

---
+++ 03_javascritp_avanzado/3.2.1.ejercicios_david/script.js
@@ -0,0 +1,94 @@
+//1. ¿Cuál es la salida de este código?
+//console.log('start'); //sincrono
+//const promise1 = new Promise((resolve, reject) => {
+//console.log(1)
+//})
+//console.log('end');
+//*2. ¿Cuál es la salida de este código?
+//console.log('start');
+//const promise1 = new Promise((resolve, reject) => {
+//console.log(1)
+//resolve(2)
+//})
+//promise1.then(res => {
+//console.log(res)
+//})
+//console.log('end');
+//3. ¿Cuál es la salida de este código?
+//console.log('start');
+//const promise1 = new Promise((resolve, reject) => {
+//console.log(1)
+//resolve(2)
+//console.log(3)
+//})
+//promise1.then(res => {
+//console.log(res)
+//})
+//console.log('end');
+//4. ¿Cuál es la salida de este código?
+//console.log('start');
+//const promise1 = new Promise((resolve, reject) => {
+//console.log(1)
+//})
+//promise1.then(res => {
+//console.log(2)
+//})
+//console.log('end');
+//5. ¿Cuál es la salida de este código?
+console.log('start') //sincrono
+const fn = () => (new Promise((resolve, reject) => {
+console.log(1); //sincrono
+resolve('success')
+}))
+console.log('middle') 
+fn().then(res => {
+console.log(res)
+})
+console.log('end')
+//6. ¿Cuál es la salida de este código?
+//console.log('start')
+//Promise.resolve(1).then((res) => {
+//console.log(res)
+//})
+//Promise.resolve(2).then((res) => {
+//console.log(res)
+//})
+//console.log('end')
+//7. ¿Cuál es la salida de este código?
+//console.log('start')
+//setTimeout(() => {
+//console.log('setTimeout')
+//})
+//Promise.resolve().then(() => {
+//console.log('resolve')
+//})
+//console.log('end')
+//8. ¿Cuál es la salida de este código?
+//const promise = new Promise((resolve, reject) => {
+//console.log(1);
+//setTimeout(() => {
+//console.log("timerStart");
+//resolve("success");
+//console.log("timerEnd");
+//}, 0);
+//console.log(2);
+//});
+//promise.then((res) => {
+//console.log(res);
+//});
+//console.log(4);
+//9. ¿Cuál es la salida de este código?
+//console.log('start');
+//const promise1 = Promise.resolve().then(() => {
+//console.log('promise1');
+//const timer2 = setTimeout(() => {
+//console.log('timer2')
+//}, 0)
+//});
+//const timer1 = setTimeout(() => {
+//console.log('timer1')
+//const promise2 = Promise.resolve().then(() => {
+//console.log('promise2')
+//})
+//}, 0)
+//console.log('end');
