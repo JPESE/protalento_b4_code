e9cb8e4e829f66a2c6eee26303c1ac2fb35d8e2d
JOHANNA <pesemmarea@hotmail.com>
2023-03-28 21:06:47 -0500

    pokemon

--- 03_javascritp_avanzado/pokedex/index.js
+++ 03_javascritp_avanzado/pokedex/index.js
@@ -1,40 +1,64 @@
-/*const request = require('request');
+const listaPokemon = document.querySelector("#listaPokemon");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-request({
-    method: "GET",
-    url: "https://storage.googleapis.com/campus-cvs/00000000000-images-lectures/pokemons.json"
-}, function ( error, response, body) {
-    console.log(body);
-    const bodyJSON = JSON.parse(body);
-    console.log(bodyJSON);
-})*/
-const crearElemento = (nombre, image) => {
-    const personajeElement = document.createElement("div");
-    const imagenElement = document.createElement("img");
-    imagenElement.src = image;
-const nombrePersonaje = document.createTextNode(nombre);
-personajeElement.appendChild(nombrePersonaje);
-personajeElement.appendChild(imagenElement);
-return personajeElement;
+for (let i = 1; i <= 151; i++) {
+    fetch(URL + i)
+    .then((response) => response.json())
+    .then(data => )
 }
 
-fetch('https://pokeapi.co/api/v2/pokemon')
-.then(response => response.json())
-.then(data => {
-console.log(data);
-data.results.map((elemento) => {
 
-   fetch(elemento.url)
-   .then(response => response.json())
-   .then(dataIndividual => {
-    console.log(dataIndividual);
-   })
-    console.log('elemento', elemento.url);
-})
-})
-.catch(error => {
-    console.error(error);
-});
-function mostrarPokemones (name1){
-console.log(name1)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const request = require('request');
+
+// request({
+//     method: "GET",
+//     url: "https://storage.googleapis.com/campus-cvs/00000000000-images-lectures/pokemons.json"
+// }, function ( error, response, body) {
+//     console.log(body);
+//     const bodyJSON = JSON.parse(body);
+//     console.log(bodyJSON);
+// })
+//  const crearElemento = (nombre, image) => {
+//     constrsonajeElement = document.createElement("div");
+//     const imagenElement = document.createElement("img");
+//     imagenElement.src = image;
+// const nombrePersonaje = document.createTextNode(nombre);
+// personajeElement.appendChild(nombrePersonaje);
+// personajeElement.appendChild(imagenElement);
+// return personajeElement;
+// }
+
+//fetch('https://storage.googleapis.com/campus-cvs/000//00000000-images-lectures/pokemons.json')
+//.then(response => console.log(response))
+// .then(data => {
+// console.log();
+//  data.results.map((elemento) => {
+// console.log(elemento.name)
+//    fetch(elemento.url)
+//    .then(response => response.json())
+//    .then(dataIndividual => {
+//     console.log(dataIndividual);
+//    })
+   
+ //})
+//})
+//.catch(error => {
+//   console.error(error);
+//});
+//function mostrarPokemones (name1){
+//console.log(name1)}
 
