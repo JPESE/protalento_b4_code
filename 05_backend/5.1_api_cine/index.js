5392bf059d7b99b1f3ed3e9e9b06a7a3e099d968
JOHANNA <pesemmarea@hotmail.com>
2023-06-22 23:23:45 -0500

    ultima modificacion

--- 05_backend/5.1_api_cine/index.js
+++ 05_backend/5.1_api_cine/index.js
@@ -3,8 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.send('Hello World!')
 })
+
 app.listen(port, () => {
-    console.log(`example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
