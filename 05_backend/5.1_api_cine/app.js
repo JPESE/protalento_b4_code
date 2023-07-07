7346bf5b920d132837c6b5d85ec6734c37a2003c
JOHANNA <pesemmarea@hotmail.com>
2023-07-06 22:45:03 -0500

    avance proyecto final

--- 05_backend/5.1_api_cine/app.js
+++ 05_backend/5.1_api_cine/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,5 +10,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
 //control+c para matar el console log y volver a la carpeta//
 //creo la base en mongo atlas 
