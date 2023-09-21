cf613a9ce6a35c82dc547badb1ba6e743ac0e132
JOHANNA <pesemmarea@hotmail.com>
2023-09-21 16:25:19 -0500

    prueba tecnica

---
+++ prueba_tecnica/script.js
@@ -0,0 +1,83 @@
+// arreglo
+const numeros = [7, 21, 8, 2, 76,3];
+
+// Encontrar el valor mínimo utilizando Math.min() y ... (operador de programación)
+const minimo = Math.min(...numeros);
+
+console.log("El valor mínimo es:", minimo);
+
+
+
+
+
+
+
+
+
+
+
+// arreglo con diferentes numeros 
+//const numeros = [7, 21, 8, 2, 76,3];
+
+// se encuentra el valor máximo utilizando Math.max()
+//const maximo = Math.max(...numeros);
+
+//console.log("El valor máximo es:", maximo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Creo el arreglo 
+//const numeros = [5, 14, 24, 21, 1, 7];
+
+// Calcula la suma utilizando el método reduce() y el argumento acumulador 
+//const totalSuma = numeros.reduce((acumulador, numero) => acumulador + numero, 0);
+
+// Calcula el promedio
+//const promedio = totalSuma / numeros.length;
+
+//console.log("El promedio es:", promedio);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Creo un arreglo con diferentes numeros
+//const arreglo = [14, 28, 34, 76, 21, 16, 89];
+
+// Obtengo el conteo con la función length que es longitud
+//const conteoTotal = arreglo.length;
+
+//console.log("El conteo total de elementos es:", conteoTotal);
+
+
+
+
+
+
+// primero que todo creo un arreglo con numeros
+//const numeros = [5, 8, 3, 1, 7];
+
+// Calculo la suma total utilizando el método reduce() y utilizo una función flecha
+//para reducir el codigo
+//const totalSuma = numeros.reduce((acumulador, numero) => acumulador + numero, 0);
+
+//console.log("La suma total es:", totalSuma);
